dbe9fb15b9df2e83f732905c444f0d49bfda6244
Karin <60923415+Warai-kiki@users.noreply.github.com>
2020-12-02 13:13:07 +0200

    commit 4

---
+++ Task_8_new/Task_8_new/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_8_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
